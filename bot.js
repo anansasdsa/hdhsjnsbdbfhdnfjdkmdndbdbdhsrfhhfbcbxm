d18b9ffee31a771c5a2bd743d847df03d2d87877
anansasdsa <41487794+anansasdsa@users.noreply.github.com>
2018-09-14 22:15:07 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ fetchAllMembers: true });
+const fs = require("fs");
+const userData = JSON.parse(fs.readFileSync('./userData.json', 'utf8'));
+const Canvas = require('canvas')
+const jimp = require('jimp')
+const moment = require('moment');
+const dateFormat = require('dateformat');
+let done = {};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`capricon`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
